f7f4456ecdaf61bf301bf9afb451695d69a0ad68
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-06-21 13:32:00 +0200

    added journal entry validations

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -4,6 +4,6 @@ class JournalEntry < ApplicationRecord
   belongs_to :accounting_code
   belongs_to :accounting_entry
 
-	validates :amount
+	validates :amount, presence: true, numericality: { greater_than: 0 }, allow_blank: false, allow_nil: false, on: :create, on: :update
 	validates :post_type, presence: true, inclusion: { in: POST_TYPES }
 end
