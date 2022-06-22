0dad66223e5e449590461ef1571e96fa092be00d
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-06-22 14:59:32 +0200

    New styling of all pages and expandable dropdown and added roboto font family

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -6,4 +6,12 @@ class JournalEntry < ApplicationRecord
 
 	validates :amount, presence: true, numericality: { greater_than: 0 }, allow_blank: false, allow_nil: false, on: :create, on: :update
 	validates :post_type, presence: true, inclusion: { in: POST_TYPES }
+
+	def is_debit?
+		self.post_type == "DR"
+	end
+
+	def is_credit?
+		self.post_type == "CR"
+	end
 end
